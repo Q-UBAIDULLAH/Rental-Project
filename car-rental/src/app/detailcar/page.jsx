576740ea0d28c1cc89fbc546245a3671a952633b
Q-UBAIDULLAH <qaziubaidullah5@gmail.com>
2025-03-18 00:19:59 +0500

    created

---
+++ car-rental/src/app/detailcar/page.jsx
@@ -0,0 +1,220 @@
+import { IoStarSharp } from "react-icons/io5";
+import { FcLike } from "react-icons/fc";
+import { BsFillFuelPumpFill } from "react-icons/bs";
+import { RxColorWheel } from "react-icons/rx";
+import { MdPeopleAlt } from "react-icons/md";
+import { CiHeart } from "react-icons/ci";
+import Slidebar from "../slidebar/page"
+// import Jeff from '../assets/CEOAmazon.jpg'
+// import Elonmusk from '../assets/CEOTesla.jpg'
+const DetailCar=()=>{
+    return(
+        <>
+        <Slidebar></Slidebar>
+ <div className="flex-col  gap-6 m-4 md:relative md:left-3 bg-orange-500 md:flex md:flex-row rounded-lg">
+
+
+
+<div className=" bg-[#c72235]  rounded-[10px] ">
+
+  
+<div className="bg-[#3563E9] w-full h-full  rounded-[10px] md:w-96 md:h-64">
+
+<div className="w-72 h-36 mt-3 ml-4 ">
+
+<div className="">
+
+<h5 className="text-lg font-semibold  text-white">Sports car with the best design and acceleration</h5>
+<h5 className="text-[16px] font-[400] text-white mt-4">Safety and comfort while driving a 
+futuristic and elegant sports car</h5>
+</div>
+
+
+
+</div>
+
+
+
+ <div className="w-full h-36 -mt-4 flex items-center justify-center   mirror-x or scale-x-[-1]">
+    <img className="w-60  sm:mb-7 md:mb-0 "  src="https://s3-alpha-sig.figma.com/img/702f/356e/48fe531e6fd2626c5d1041dbfcde3341?Expires=1742774400&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=KcWv2IRSajtPvZQSJDMYJ7okXpewrye2CEmHipx6JwFeyAEHZWZrd3NHLAijQ8Pyf635yFHv1GJoyn8wq~DLXzHuV5VfSfNrDdBOISy4hQrpUbA8dymWepI1ogFBvj5e1RPfG2HDdcN1baABXQ16wXNTNb7Dqe83Fi96UnzitFZN7kgtJfASYJeNIsk7GObvRLhq1V2CkWp37KK~s7z4P8E60k-uVDIp5AzxyFSTRxxzy~jv3pPfLFV7alnQHnFNmTIxOj5HnFwElYayhRSQUyimAvzBStw8lWd7P1EgGo41asixbnGrJqttBSVIqq-pNcI4QnH2S~5tVIZ7Q16~6A__" alt="" />
+</div> 
+
+
+</div>
+
+
+ 
+<div className="flex gap-3 w-full h-full  mt-4 md:w-96">
+    <div className="w-64 h-28 rounded-[10px] bg-[#3563E9] mirror-x or scale-x-[-1]"> <img className="w-full h-28"  src="https://s3-alpha-sig.figma.com/img/702f/356e/48fe531e6fd2626c5d1041dbfcde3341?Expires=1742774400&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=KcWv2IRSajtPvZQSJDMYJ7okXpewrye2CEmHipx6JwFeyAEHZWZrd3NHLAijQ8Pyf635yFHv1GJoyn8wq~DLXzHuV5VfSfNrDdBOISy4hQrpUbA8dymWepI1ogFBvj5e1RPfG2HDdcN1baABXQ16wXNTNb7Dqe83Fi96UnzitFZN7kgtJfASYJeNIsk7GObvRLhq1V2CkWp37KK~s7z4P8E60k-uVDIp5AzxyFSTRxxzy~jv3pPfLFV7alnQHnFNmTIxOj5HnFwElYayhRSQUyimAvzBStw8lWd7P1EgGo41asixbnGrJqttBSVIqq-pNcI4QnH2S~5tVIZ7Q16~6A__" alt="" />
+    </div>
+    <div  className="w-64 h-28  rounded-[10px] ">
+        <img className="w-full h-28 rounded-[10px] " src="https://s3-alpha-sig.figma.com/img/2a70/cf86/a73f1fdfda8a78629de1874016c972fe?Expires=1742774400&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=qjBDlPoQYC2GxuUCI5pkDyujIikvCAa3nWEH4V0fVKdow~o3nDIe6LdQaIrMqpAv3QCMndM5QKkJ9ASAxxqXkOZsLO-wOhptOVGb9yeWReIKYNQlmxb-dz3zPuzsC4BKwwzljT0xRymY1aqWZ1X~I9wsQCGsiVmeq7jfu4WW1MiPzbGrABGUOxYh5KRmO0l1RC8x7SES16uuk-xNeLf3S0tPlyiYhAC2bEi~NtNker4yGTNsrXLg9wOacrdGRIJDeGS3T2n8HbBc81pkooQ--YBSJVXaolhhDdNnMo1Ek~Mu6a9vC9pT5eGJMECwlsAVZ~PuZS0S2~CK7A712aUuSA__" alt="" />
+    </div>
+    <div  className="w-64 h-28 rounded-[10px] ">
+        <img className="w-full h-28 rounded-[10px] "   src="https://s3-alpha-sig.figma.com/img/b79c/e842/46da5a33b100b5e944d576a984fba3e1?Expires=1742774400&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=N8VO5k9EnDru6rUQNhMV60GaRAqAsiugiW-7U5rR8Ze0bbxsflc-ez~psSPxuaDsqtmOfYLsqkRd8kXh~c2soW0QcRpOSkypsbBTUXstNIjttNMT075AM8gCn6yE7c~zIYS1c1AEtEyngneF~MKWQJN1HWpEHRDWwwc7jHrK-3Q-6VQ5pcuK1vF1zy3r9pjOiUEN3eulr6Oof6lMK16HpHw8C9uZELivAkexzn9qByZsO-leh-ziz7qlT7MAVHrcK8JAElgeiBb6jnkpyZwrdJ5foEdRDbwp~v4a7VU23TzyAl8crop9rhLl~Ok4EAFDwVY5UA455RTvFqfXjB2UlQ__" alt="" />
+    </div>
+</div> 
+
+</div>
+
+
+
+
+
+
+
+<div className="w-full bg-red-700 rounded-[10px]  mt-3 md:w-96">
+
+    <div className=" w-[220px] h-[72px]  ml-4 float-left mt-3">
+        <h1 className="font-[700] text-[22px] float-left ">Nissan GT-R</h1>
+       <div className="flex gap-[1px] mt-1 float-left">
+        <span className="text-[#FBAD39]"> <IoStarSharp  fontSize={13}/></span>
+        <span className="text-[#FBAD39]"> <IoStarSharp  fontSize={13}/></span>
+        <span className="text-[#FBAD39]"> <IoStarSharp  fontSize={13}/></span>
+        <span className="text-[#FBAD39]"> <IoStarSharp  fontSize={13}/></span>
+        <span className=" text-[white]"> <IoStarSharp  fontSize={13}/></span>
+        <h5 className="text-[11px] ml-2 mt-[-1px] font-[500]  text-[#596780]">440+ Reviewer</h5>
+       
+      </div>
+     
+    </div>
+ 
+     {/* <div className=" w-[24px] h-[24px] mt-4 mr-4 float-right">
+      <FcLike className="text-[21px] "/>
+      </div>  */}
+ 
+ <div className="clear-both"></div>
+
+<div className="ml-4  ">
+<p className="text-semibold font-[400] text-[#596780] leading-8">NISMO has become the embodiment of Nissan's outstanding performance, inspired by the most unforgiving proving ground, the "race track".</p>
+</div>
+<div className="grid grid-cols-4 gap-4 ml-2 mr-2 mt-4 ">
+  <p className="text-[#90A3BF] text-[17px] font-[400] text-center">TypeCar</p>
+  <p className="text-center font-[600]">Sport</p>
+  <p className="text-[#90A3BF] text-[17px] font-[400] text-center">Capacity</p>
+  <p className="text-center font-[600]">2Person</p>
+
+  <p className="text-[#90A3BF] text-[17px] font-[400] text-center">Steering</p>
+  <p className="text-center font-[600]">Manual</p>
+  <p className="text-[#90A3BF] text-[17px] font-[400] text-center">Gasoline</p>
+  <p className="text-center font-[600]">70L</p>
+</div>
+
+<div className="flex  justify-between mt-8 pb-4 ml-3 mr-3">
+   <div className="w-[116px] h-[44px]  ">
+    <h2 className="text-[20px] font-[700]">$80.00/<span className="text-[14px] font-[700] text-[#90A3BF]">day</span></h2>
+    <h2> <del className="text-[#90A3BF] text-[14px] font-[700]"> $100.00</del></h2>
+    </div> 
+    <button className=" font-[600] w-32 h-12 rounded-[4px] bg-[#3563E9] text-[#FFFFFF]">Rent Now</button>
+</div>
+
+
+</div> 
+
+
+</div>
+
+
+<div className=" m-4 bg-red-300 rounded-lg  md:flex  ">
+
+ <div className="w-full h-full  p-4  rounded-[10px] ">
+
+  <div className="flex gap-3  pt-3 w-32 h-12 ">
+    <h3 className="font-[600]">Reviews</h3>
+    <p className="w-[34px] h-[23px] rounded-[5px] text-[white] text-center bg-blue-700">13</p>
+  </div>
+
+
+<div className="w-full h-[124px]   mt-6">
+    <div className="flex justify-between">
+        <div className="flex">
+              <div className=" rounded-full md:w-[43px] md:h-[45px] md:rounded-[60px]   md:flex md:items-center md:justify-center ">
+                <img className="w-12 h-12 rounded-full md:w-[39px] md:h-[40px]
+               md:rounded-[20px]" src="https://s3-alpha-sig.figma.com/img/9b76/706a/6ff04d2c5ee05f69f07eda85ba8c1846?Expires=1742774400&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=NNTxFKehR2D7lEoqczMIX40sqgetMyJvaj51zDQz5rxHAB8HWkaTfVeXUNRSnoMrDsO9QOWWZR9emea-LRiHFjbKhaXqKIS1uOQkwRrVtFPJR2u9mbFuZzIjMWOM9IdV~M-pST7qGC1hKWyDp~99iE8eXaEOMGfypwcfcdunj8fvTVgK-xIVX5IbanZyh1HRiQSF71yqIXmhqdW9bKIA2jYV6tq~bsX401MUoXZ11u~ELcpexzdFmKdLUckAdvcot6Xc8tzJ6NbHF1-96~zX366LJ0Knb0l-oBmg3d2WOBUg2umEkUql9V3VJHeozVPKmtUMqU35ERIWEvHS74AKeA__" alt="" />
+               </div>
+  
+       
+        <div className=" flex-col gap-4 mt-1 ml-3 items-center justify-center ">
+        <h3 className="text-[#1A202C] font-[700]">Jeff Bezos</h3> 
+            <h3 className="text-[#90A3BF] mt-1 font-[400] text-[12px]">CEO at Amazon</h3></div>
+        </div>
+       
+     
+        <div className="">
+        <p className="text-[#90A3BF] font-[400]">1 Feb 2025</p>
+       
+    <div className="">
+    
+       <div className="flex gap-[3px] mt-1.5 ">
+        <span className="text-[#FBAD39]"> <IoStarSharp  fontSize={14}/></span>
+        <span className="text-[#FBAD39]"> <IoStarSharp  fontSize={14}/></span>
+        <span className="text-[#FBAD39]"> <IoStarSharp  fontSize={14}/></span>
+        <span className="text-[#FBAD39]"> <IoStarSharp  fontSize={14}/></span>
+        <span className="text-[#bbbaba] "> <IoStarSharp  fontSize={14}/></span>
+        
+       
+      </div>
+     
+    </div>
+    </div>
+    </div>
+    <p className="text-[#596780] font-normal ml-14 mt-3 line-clamp-2">We are very happy with the service from the MORENT App. Morent has a low price and also a large variety of cars with good and comfortable facilities. In addition, the service provided by the officers is also very friendly and very polite.</p>
+    
+
+   
+ 
+
+</div>
+
+
+
+
+
+<div className="w-full h-[124px]   mt-3">
+    <div className="flex justify-between">
+        <div className="flex">
+              <div className=" rounded-full md:w-[43px] md:h-[45px] md:rounded-[60px]   md:flex md:items-center md:justify-center ">
+                <img className="w-12 h-12 rounded-full md:w-[39px] md:h-[40px]
+               md:rounded-[20px]" src="https://s3-alpha-sig.figma.com/img/1f59/2390/d69545d7cf9ff5be12e2248ebf54993a?Expires=1742774400&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=ZcSf59ClVSckbloB~WyYf6rfSuF4Er8xd2558uuCeDp~BB0d3qcAA45fRyukTOtt2sMq9b5S-OE4XlOKyXqCAT9lQZtyGNW-vz9fj62j03GDDaiLrFxCeQRJwLSvnIn~RdDlGQiHpAfRkKgAJgVll16VIVrBE5STc4pVa2VJdIVLM6lYwtK7Qq04YKFlYIxO72D2m55JvN1Ufl-ZYFpCZduc-islrYRcbgwendwsL~E-ITQ0uGmQh4GOlVl7JT5RCGe2nNYClsQ0Q8B-vdXu27JZdIBjDszyL5Qe-EX-hRJxOq9W5ff7kzG8CaeAI4AVjoL9gBHi~I-Z5St98Srb~A__" alt="" />
+               </div>
+  
+       
+        <div className=" flex-col gap-4 mt-1 ml-3 items-center justify-center ">
+        <h3 className="text-[#1A202C] font-[700]">Elon Musk</h3> 
+            <h3 className="text-[#90A3BF] mt-1 font-[400] text-[12px]">CEO at Tesla Motors</h3></div>
+        </div>
+       
+     
+        <div className="">
+        <p className="text-[#90A3BF] font-[400]">1 Feb 2025</p>
+       
+    <div className="">
+    
+       <div className="flex gap-[3px] mt-1.5 ">
+        <span className="text-[#FBAD39]"> <IoStarSharp  fontSize={14}/></span>
+        <span className="text-[#FBAD39]"> <IoStarSharp  fontSize={14}/></span>
+        <span className="text-[#FBAD39]"> <IoStarSharp  fontSize={14}/></span>
+        <span className="text-[#FBAD39]"> <IoStarSharp  fontSize={14}/></span>
+        <span className="text-[#bbbaba] "> <IoStarSharp  fontSize={14}/></span>
+        
+       
+      </div>
+     
+    </div>
+    </div>
+    </div>
+    <p className="text-[#596780] font-normal ml-14 mt-3 line-clamp-2">We are greatly helped by the services of the MORENT Application.Morent has low prices and also a wide variety of cars with good and comfortable facilities.In addition, the service provided by the officers is also very friendly and very polite.</p>
+    
+</div> 
+<div className="text-center mt-3 ">
+    <select className="text-[#90A3BF] ">
+        <option value="" className="">Show All</option>
+    </select>
+</div>
+</div>
+
+
+
+</div>
+</>)}
+export default DetailCar
